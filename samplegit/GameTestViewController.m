d62f7ccc40120c46dcff871dd428508e8ba209ae
toolbox-Inc <ray.c.amour@hotmail.com>
2013-12-09 23:32:30 +0100

    record button

--- samplegit/GameTestViewController.m
+++ samplegit/GameTestViewController.m
@@ -23,12 +23,15 @@
     [self.stopButton setEnabled:NO];
     [self.playButton setEnabled:NO];
     
+    NSError *error = [[NSError alloc]init];
+
+    
     NSArray *pathComponent = [NSArray arrayWithObjects:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)lastObject],@"myAudioMemo.m4a",nil];
 	
     NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponent];
     
     AVAudioSession *session = [AVAudioSession sharedInstance];
-    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
     
     NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc]init];
     
@@ -36,9 +39,10 @@
     [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
     [recordSetting setValue:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
     
-    self.audioRecorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    self.audioRecorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:&error];
     self.audioRecorder.delegate = self;
     self.audioRecorder.meteringEnabled = YES;
+    
     [self.audioRecorder prepareToRecord];
     
 }
@@ -49,9 +53,30 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)recordButton:(UIButton *)sender {
-}
+
 - (IBAction)recordPauseTapped:(UIButton *)sender {
+    
+    
+    if(self.audioPlayer.playing) {
+        [self.audioPlayer stop];
+    }
+    
+    if (!self.audioRecorder.recording) {
+        
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        [self.audioRecorder record];
+        [self.recordPauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+
+    }
+    else {
+        [self.audioRecorder pause];
+        [self.recordPauseButton setTitle:@"Record" forState:UIControlStateNormal];
+    }
+    
+    [self.stopButton setEnabled:YES];
+    [self.playButton setEnabled:NO];
+
 }
 
 - (IBAction)stopTapped:(UIButton *)sender {
@@ -60,3 +85,32 @@
 - (IBAction)playTapped:(UIButton *)sender {
 }
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
